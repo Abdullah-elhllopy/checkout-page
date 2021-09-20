80b91b55b250b102de9bf2db20f8b3d7d937cfc8
Abdullah-elhllopy <60758904+Abdullah-elhllopy@users.noreply.github.com>
2021-09-20 16:21:58 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Navber from './components/Header/navbar/navbar';
+import ShoppingChart from './components/Header/shopping/shopping-chart';
+function App() {
+  return (
+    <div className="App">
+     <Navber/>
+     <ShoppingChart/>
+    </div>
+  );
+}
+
+export default App;
